00eebfbb438ad7eb814ffcacf14db9024082e762
Steve Biko <biko.nyamai@gmail.com>
2018-07-26 09:05:33 +0300

    Parameterised scrapDuration @application.scrap_cycle

--- src/main/java/home/bikxs/jumiascrapper/Application.java
+++ src/main/java/home/bikxs/jumiascrapper/Application.java
@@ -1,24 +1,25 @@
 package home.bikxs.jumiascrapper;
 
-import home.bikxs.jumiascrapper.data.Item;
 import home.bikxs.jumiascrapper.data.SubCategory;
 import home.bikxs.jumiascrapper.service.ScrappingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 	private static Logger LOGGER = LoggerFactory.getLogger(Application.class);
+	@Value("${application.scrap_cycle}")
+	private Long scrapDuration;
 	@Autowired
 	private ScrappingService scrappingService;
 	public static void main(String[] args) {
@@ -31,13 +32,15 @@ public class Application implements CommandLineRunner {
 		scrappingService.startFileUpdater();
 
 		List<SubCategory> subcategories = scrappingService.scrapeCategories();
-		Collections.shuffle(subcategories);
-		for (SubCategory subCategory :
-				subcategories) {
-			//System.out.println(subCategory.toString());
-			scrappingService.scrapeCategories(subCategory);
 
-		}
-
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				LOGGER.info("Scrapping all items every 5 mins task");
+				Collections.shuffle(subcategories);
+				scrappingService.scrapeItems(subcategories);
+			}
+		}, 0, scrapDuration);
 	}
 }
--- src/main/java/home/bikxs/jumiascrapper/service/ScrappingService.java
+++ src/main/java/home/bikxs/jumiascrapper/service/ScrappingService.java
@@ -35,134 +35,135 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 @Service
 public class ScrappingService {
-    private static Logger LOGGER = LoggerFactory.getLogger(ScrappingService.class);
-    private final RestTemplate restTemplate = restTemplate();
-    private final String BASE_URL = "https://www.jumia.co.ke/";
-    private final HttpHeaders headers = createHeadeers();
-    private volatile boolean finished = false;
-    private ConcurrentMap<String, Item> itemConcurrentMap = new ConcurrentHashMap<>();
-    private ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(200);
-    @Value("${application.deepsearch}")
-    private Boolean deep_search;
+	private static Logger LOGGER = LoggerFactory.getLogger(ScrappingService.class);
+	private final RestTemplate restTemplate = restTemplate();
+	private final String BASE_URL = "https://www.jumia.co.ke/";
+	private final HttpHeaders headers = createHeadeers();
+	private volatile boolean finished = false;
+	private ConcurrentMap<String, Item> itemConcurrentMap = new ConcurrentHashMap<>();
+	private ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(200);
+	@Value("${application.deepsearch}")
+	private Boolean deep_search;
 
-    public List<SubCategory> scrapeCategories() {
-        LOGGER.info("Started scrape");
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-        ResponseEntity<String> respEntity = restTemplate.exchange(BASE_URL, HttpMethod.GET, entity, String.class);
-        String queired = respEntity.getBody();
-        Document doc = Jsoup.parse(queired);
-        Elements catElements = doc.getElementsByClass("subcategory");
-        List<SubCategory> subCategoryList = new ArrayList<>();
-        for (Element catElement :
-                catElements) {
-            String name = catElement.text();
-            String href = catElement.attr("href");
-            SubCategory subCategory = new SubCategory(name, href);
+	public List<SubCategory> scrapeCategories() {
+		LOGGER.info("Started scrape");
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		ResponseEntity<String> respEntity = restTemplate.exchange(BASE_URL, HttpMethod.GET, entity, String.class);
+		String queired = respEntity.getBody();
+		Document doc = Jsoup.parse(queired);
+		Elements catElements = doc.getElementsByClass("subcategory");
+		List<SubCategory> subCategoryList = new ArrayList<>();
+		for (Element catElement :
+				catElements) {
+			String name = catElement.text();
+			String href = catElement.attr("href");
+			SubCategory subCategory = new SubCategory(name, href);
 
-            subCategoryList.add(subCategory);
-            //System.out.println(subCategory.toString());
-        }
-        LOGGER.info("Found " + subCategoryList.size() + " subcategories");
-        return subCategoryList;
-    }
+			subCategoryList.add(subCategory);
+			//System.out.println(subCategory.toString());
+		}
+		LOGGER.info("Found " + subCategoryList.size() + " subcategories");
+		return subCategoryList;
+	}
 
 
-    private static HttpHeaders createHeadeers() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Host", "www.jumia.co.ke");
-        headers.add("User-Agent", "Mozilla/5.0 (Windows NT 6.2; rv:20.0) Gecko/20121202 Firefox/20.0");
-        headers.add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-        headers.add("Accept-Language", "en-US,en;q=0.5");
-        headers.add("Referer", "https://www.jumia.co.ke");
-        headers.add("Connection", "keep-alive");
-        return headers;
-    }
+	private static HttpHeaders createHeadeers() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Host", "www.jumia.co.ke");
+		headers.add("User-Agent", "Mozilla/5.0 (Windows NT 6.2; rv:20.0) Gecko/20121202 Firefox/20.0");
+		headers.add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		headers.add("Accept-Language", "en-US,en;q=0.5");
+		headers.add("Referer", "https://www.jumia.co.ke");
+		headers.add("Connection", "keep-alive");
+		return headers;
+	}
 
-    private RestTemplate restTemplate() {
+	private RestTemplate restTemplate() {
 
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
 
 		InetSocketAddress address = new InetSocketAddress("172.23.12.226", 4145);
 		try {
-			if(address.getAddress().isReachable(5000)){
+			if (address.getAddress().isReachable(5000)) {
 				Proxy proxy = new Proxy(Proxy.Type.HTTP, address);
 				requestFactory.setProxy(proxy);
 			}
 		} catch (IOException e) {
-			System.out.println(address.getAddress().getHostAddress()+ ": " + e.getMessage());;
+			System.out.println(address.getAddress().getHostAddress() + ": " + e.getMessage());
+			;
 		}
 
-        return new RestTemplate(requestFactory);
-    }
+		return new RestTemplate(requestFactory);
+	}
 
-    public void scrapeCategories(SubCategory subCategory) {
-        executor.execute(new SubCategoryScrapperThread(subCategory, headers, itemConcurrentMap, restTemplate,deep_search));
-    }
+	public void scrapeCategories(SubCategory subCategory) {
+		executor.execute(new SubCategoryScrapperThread(subCategory, headers, itemConcurrentMap, restTemplate, deep_search));
+	}
 
-    public void startFileUpdater() {
-        try {
-            FileUpdater updater = new FileUpdater();
-            updater.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	public void startFileUpdater() {
+		try {
+			FileUpdater updater = new FileUpdater();
+			updater.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    }
+	}
+	public void scrapeItems(List<SubCategory> subcategories) {
+		LOGGER.info("Started search @" + Calendar.getInstance().getTime());
+		for (SubCategory subCategory :
+				subcategories) {
+			scrapeCategories(subCategory);
+		}
+	}
 
-    public class FileUpdater extends Thread implements Runnable {
-        PrintWriter pw;
-        PrintWriter pwFound;
+	public class FileUpdater extends Thread implements Runnable {
+		PrintWriter pw;
+		PrintWriter pwFound;
 
-        public FileUpdater() throws IOException {
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHH");
-            String timeStamp = simpleDateFormat.format(Calendar.getInstance().getTime());
-            pw = new PrintWriter(new FileWriter(new File("jumia_items_" + timeStamp + ".csv")));
-            pwFound = new PrintWriter(new FileWriter(new File("found_treasures_" + timeStamp + ".csv")));
+		public FileUpdater() throws IOException {
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHH");
+			String timeStamp = simpleDateFormat.format(Calendar.getInstance().getTime());
+			pw = new PrintWriter(new FileWriter(new File("jumia_items_" + timeStamp + ".csv")));
+			pwFound = new PrintWriter(new FileWriter(new File("found_treasures_" + timeStamp + ".csv")));
 
-            pw.println("Subcatory;Item;Price;Discount;href");
-            pwFound.println("Subcatory;Item;Price;Discount;href");
-        }
+			pw.println("Subcatory;Item;Price;Discount;href");
+			pwFound.println("Subcatory;Item;Price;Discount;href");
+		}
 
-        @Override
-        public void run() {
+		@Override
+		public void run() {
 
-            while (!finished) {
-                if (executor.getActiveCount() == 0 && executor.getCompletedTaskCount() > 0) {
-                    if (executor.getCompletedTaskCount() == executor.getTaskCount())
-                        finished = true;
-                }
-                try {
-                    Thread.sleep(15000);
-                } catch (InterruptedException e) {
+			while (!finished) {
+				if (executor.getActiveCount() == 0 && executor.getCompletedTaskCount() > 0) {
+					if (executor.getCompletedTaskCount() == executor.getTaskCount()) {
+						finished = true;
+						LOGGER.info("Finished search @" + Calendar.getInstance().getTime());
+						break;
 
-                }
-                List<Item> items = new ArrayList<>(itemConcurrentMap.values());
-                itemConcurrentMap.clear();
-                for (Item item :
-                        items) {
-                    if (item == null) continue;
-                    pw.println(item.toCSVString());
-                    if (item.getDiscount() >= 99.5) {
-                        pwFound.println(item.toCSVString());
-                    }
-                }
-                System.out.println(executor.getCompletedTaskCount() + "/" + executor.getTaskCount() + " tasks completed.");
-                if (items.isEmpty()) {
+					}
+				}
+				try {
+					Thread.sleep(15000);
+				} catch (InterruptedException e) {
 
-                    System.out.println("Execution Queue");
-                    for (Runnable runnable :
-                            executor.getQueue()) {
-                        SubCategoryScrapperThread scst = (SubCategoryScrapperThread) runnable;
-                        System.out.println("\t" + scst.getSubCategory().toString());
-                    }
-                }
-                ;
+				}
+				List<Item> items = new ArrayList<>(itemConcurrentMap.values());
+				itemConcurrentMap.clear();
+				for (Item item :
+						items) {
+					if (item == null) continue;
+					pw.println(item.toCSVString());
+					if (item.getDiscount() >= 99.5) {
+						pwFound.println(item.toCSVString());
+					}
+				}
+				LOGGER.info("Tasks Active=" + executor.getActiveCount() + " Completed=" + executor.getCompletedTaskCount() + " Total=" + executor.getTaskCount() + "");
+			}
 
-            }
+			pw.close();
+			pwFound.close();
 
-            pw.close();
-            pwFound.close();
-
-        }
-    }
+		}
+	}
 }
